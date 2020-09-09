b596283e7d1cfc27ca5e306a6fa88b12f64a5a8a
Rashna Maharjan <rashnamaharjan@Rashnas-MacBook-Air.local>
2020-09-09 15:39:47 -0400

    Renamed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world ! from github.com..");
         System.out.print("Rashna Maharjan");
+        System.out.println("Welcome to IT Sutra");
     }
 }
